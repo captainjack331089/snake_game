659d3edd8ad9bbbb4f9c12936d32d1d3bbfd605e
captainJack33 <qfhjack@gmail.com>
2022-02-12 22:07:58 -0800

    checkin

---
+++ TODO.py
@@ -0,0 +1,8 @@
+## TODO: Create a snake body
+## TODO: Move the snake
+## TODO: Control the snake
+## TODO: Create Food
+## TODO: Detect collision with food
+## TODO: Create a score board
+## TODO: Detect collision with wall
+## TODO: Detect collision with tail
